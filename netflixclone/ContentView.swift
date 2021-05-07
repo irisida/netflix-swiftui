79f1054dbf94f3626e7bd0557e4083747bcb7909
irisida <20445648+irisida@users.noreply.github.com>
2021-05-07 23:12:35 +0100

    Initial Commit

---
+++ netflixclone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  netflixclone
+//
+//  Created by ed on 07/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ netflixclone/netflixcloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflixcloneApp.swift
+//  netflixclone
+//
+//  Created by ed on 07/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct netflixcloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
