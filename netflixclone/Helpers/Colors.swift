fdb807dac6dd49779e0a60c439a7a89cddecdb03
irisida <20445648+irisida@users.noreply.github.com>
2021-05-16 23:05:10 +0100

    first drop of the search bar

---
+++ netflixclone/Helpers/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  netflixclone
+//
+//  Created by ed on 16/05/2021.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let graySearchBackground = Color("gray_search_background")
+    static let graySearchText = Color("gray_search_text")
+}
---
+++ netflixclone/Search/SearchBar.swift
@@ -0,0 +1,95 @@
+//
+//  SearchBar.swift
+//  netflixclone
+//
+//  Created by ed on 16/05/2021.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    
+    @Binding var text: String
+    
+    @State private var isEditing = true
+    
+    @Binding var isLoading: Bool
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            Color.graySearchBackground
+                .frame(width: 280, height: 38)
+                .cornerRadius(8)
+                
+            
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(Color.graySearchText)
+                    .padding(.leading, 10)
+                    
+                
+                TextField("Search", text: $text)
+                    .foregroundColor(.white)
+                    .accentColor(.graySearchText)
+                    .cornerRadius(8)
+                    .padding(8)
+                    .padding(.leading, -8)
+                    .background(Color.graySearchBackground)
+                    .onTapGesture(perform: {
+                        isEditing = true
+                    })
+                    .cornerRadius(8)
+                    .frame(height: 34)
+                
+                if !text.isEmpty {
+                    if isLoading {
+                        Button(action: {
+                            text = ""
+                        }, label: {
+                            ActivityIndicator(style: .medium, animate: .constant(true))
+                                .configure {
+                                    $0.color = .white
+                                }
+                        })
+                        .frame(width: 35, height: 34)
+                        .padding(.trailing, 18)
+                    } else {
+                        Button(action: {
+                            text = ""
+                        }, label: {
+                            Image(systemName: "xmark.circle.fill")
+                                .foregroundColor(.graySearchText)
+                                .frame(width: 35, height: 34)
+                        })
+                        .padding(.trailing, 18)
+                    }
+                }
+                
+                
+                if isEditing {
+                    Button(action: {
+                        // clear the text, hide buttons, give up forst responder
+                        text = ""
+                        isEditing = false
+                        hideKeyboard()
+                    }, label: {
+                        Text("Cancel")
+                            .foregroundColor(.white)
+                    })
+                    .padding(.trailing, 10)
+                }
+            }
+        }
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            SearchBar(text: .constant("movie name"), isLoading: .constant(true))
+                .padding()
+        }
+    }
+}
